8504aa80b420daf4f4ddd97c62c91571818dd9f0
Deepak Sharma <deepaucksharma@gmail.com>
2024-10-29 16:05:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 
-// Tailwind CSS import
-import './index.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
